d6b66b60e9420c681e67751350ec4eab611c5a0a
DaEunShim <10ekdms01@naver.com>
2020-03-30 18:06:04 +0900

    create project

---
+++ src/test/java/caredirection/boardapi/BoardapiApplicationTests.java
@@ -0,0 +1,13 @@
+package caredirection.boardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
